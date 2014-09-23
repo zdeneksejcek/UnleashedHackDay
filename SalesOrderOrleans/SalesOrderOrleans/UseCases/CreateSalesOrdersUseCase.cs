660e289bbd717bc3d3d99ed801a19a72ecbe015d
zdeneksejcek <zdenek@sejcek.cz>
2014-09-23 13:10:37 +1200

    Added CreateOrderOrleansUseCase

---
+++ SalesOrderOrleans/SalesOrderOrleans/UseCases/CreateSalesOrdersUseCase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SalesOrderOrleans.UseCases
+{
+    public class CreateSalesOrdersUseCase
+    {
+        public IEnumerable<Guid> Execute(int number)
+        {
+            var guids = new List<Guid>();
+
+
+
+            return guids;
+        }
+    }
+}
